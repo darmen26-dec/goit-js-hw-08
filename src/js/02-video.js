f6974cb497afd3c3f04f7d665eb015cc4f561123
darmen26-dec <dariamendelewska@gmail.com>
2023-09-13 15:18:45 +0200

    update1 ex 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+};
+
+const throttledSaveTime = throttle(onPlay, 1000);
+
+player.on('timeupdate', throttledSaveTime);
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || 0);
