53e159a3aecb0a1684bed049987fac5221c6bdd5
darmen26-dec <dariamendelewska@gmail.com>
2023-09-16 00:04:26 +0200

    update ex 2 & 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,4 +14,42 @@ const throttledSaveTime = throttle(onPlay, 1000);
 
 player.on('timeupdate', throttledSaveTime);
 
-player.setCurrentTime(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || 0);
+player.on('loaded', () => {
+  player.setCurrentTime(
+    JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || 0
+  );
+});
+
+// DRUGIE ROZWIĄZANIE
+
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const iframe = document.querySelector('#vimeo-player');
+// const player = new Player(iframe);
+// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+// const onPlay = function (data) {
+//   localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+// };
+
+// const throttledSaveTime = throttle(onPlay, 1000);
+
+// player.on('timeupdate', throttledSaveTime);
+
+// player.on('loaded', () => {
+//   const savedTime = localStorage.getItem(LOCALSTORAGE_KEY);
+//   if (savedTime) {
+//     player
+//       .setCurrentTime(Number.parseFloat(savedTime)) //pamiętać, że parseFloat jest potrzebne w przypadku gdy wartość jest pobrana typu string
+//       .catch(function (error) {
+//         // zmodyfikowane z dokumentacji
+//         switch (error.name) {
+//           case 'RangeError':
+//             break;
+//           default:
+//             break;
+//         }
+//       });
+//   }
+// });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const savingLocalStorage = throttle(() => {
+  // funkcja throttle jest używana do kontrolowania częstotliwości zapisu danych do localStorage
+  const formData = {
+    // zawiera aktualne wartości pól 'email' i 'message'
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}, 500);
+
+window.addEventListener('load', () => {
+  const storedFormData = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (storedFormData) {
+    const parsedFormData = JSON.parse(storedFormData);
+    form.elements.email.value = parsedFormData.email;
+    form.elements.message.value = parsedFormData.message;
+  }
+});
+
+form.elements.email.addEventListener('input', () => {
+  savingLocalStorage();
+});
+form.elements.message.addEventListener('input', () => {
+  savingLocalStorage();
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault(); // zapobiega domyślnej akcji przycisku "Submit", która powoduje przeładowanie strony
+  console.log(LOCALSTORAGE_KEY, {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  });
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  form.reset();
+});
+
+// NOTATKI
+// throttle - Kontrola częstotliwości:
+// throttle to funkcja biblioteki lodash, która zapewnia kontrolę nad częstotliwością wykonywania innej funkcji. Działa tak, że wykonuje tę funkcję co najwyżej raz w określonym przedziale czasowym (w tym przypadku co 500 milisekund). Jeśli funkcja zostanie wywołana częściej, niż określony limit czasowy, throttle opóźnia jej wykonanie, aby nie przekraczała limitu.
+
+// Pusty nawias () - Anonimowa funkcja:
+// W kodzie używamy pustego nawiasu () w funkcji throttle, ponieważ potrzebujemy przekazać funkcję, którą chcemy kontrolować. Funkcja ta nie przyjmuje żadnych argumentów, ponieważ dostęp do pól formularza jest realizowany bezpośrednio wewnątrz funkcji savingLocalStorage
